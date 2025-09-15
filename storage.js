0d6af3af217bb7971b26b273897c13e10cd9b9ee
B Sai Umesh <saiumesh2003@gmail.com>
2025-09-15 16:15:31 +0530

    Delete storage.js

--- storage.js
+++
@@ -1,3 +0,0 @@
-export const save = (k,v)=> localStorage.setItem(k, JSON.stringify(v))
-export const load = (k)=> { try{ return JSON.parse(localStorage.getItem(k)) }catch{return null} }
-export const remove = (k)=> localStorage.removeItem(k)
